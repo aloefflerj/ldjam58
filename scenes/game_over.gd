85573a5bc3b7b1eec7bacd1910e162ac15f36dde
Júlio Cezar Koronati <jckoronati@hotmail.com>
2025-10-04 10:13:46 -0300

    feat: implements game over scene

---
+++ scenes/game_over.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready() -> void:
+	get_tree().paused = true
+
+
+func _on_retry_pressed() -> void:
+	get_tree().paused = false	
+	
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/Game.tscn")
