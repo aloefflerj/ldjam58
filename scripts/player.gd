b72b8207de25cbf293f1f25d30695fb64280bbbd
Anderson Job Loeffler <aloeffler.j@gmail.com>
2025-10-04 01:18:40 -0300

    feat: enhances bully push

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,89 +7,103 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
-enum PlayerState { IDLE, RUN, JUMP, FALL }
+enum PlayerState {IDLE, RUN, JUMP, FALL}
 
 var _state: PlayerState = PlayerState.IDLE
 var sound_played = false
+var input_enabled = true
+var got_hit = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+    handle_movement_input_behaviour()
+    hit_slow_down()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+    # Add the gravity.
+    if not is_on_floor():
+        velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if velocity.x > 0:
-		animated_sprite_2d.flip_h = false
-	
-	if velocity.x < 0:
-		animated_sprite_2d.flip_h = true
-	
-	## Example of iteraction between player and a rigid body 2d
-	var collision: KinematicCollision2D = get_last_slide_collision()
-	
-	if collision:
-		var collider_node = collision.get_collider()
-		
-		if collider_node is RigidBody2D:
-			var collision_normal: Vector2 = collision.get_normal()
-			var push_force = 100
-			var push_direction = -collision_normal * push_force
-			
-			collider_node.apply_central_impulse(push_direction)
-			
-			if not sound_played:
-				SoundManager.play_clip(sound_player, SoundManager.HIT)
-				sound_played = true
+    if velocity.x > 0:
+        animated_sprite_2d.flip_h = false
+    
+    if velocity.x < 0:
+        animated_sprite_2d.flip_h = true
+    
+    ## Example of iteraction between player and a rigid body 2d
+    var collision: KinematicCollision2D = get_last_slide_collision()
+    
+    if collision:
+        var collider_node = collision.get_collider()
+        
+        if collider_node is RigidBody2D:
+            var collision_normal: Vector2 = collision.get_normal()
+            var push_force = 100
+            var push_direction = - collision_normal * push_force
+            
+            collider_node.apply_central_impulse(push_direction)
+            
+            if not sound_played:
+                SoundManager.play_clip(sound_player, SoundManager.HIT)
+                sound_played = true
 
-				await get_tree().create_timer(0.5).timeout
-				sound_played = false
-	
-	move_and_slide()
-	calculate_states()
-	
+                await get_tree().create_timer(0.5).timeout
+                sound_played = false
+    
+    move_and_slide()
+    calculate_states()
+    
 func calculate_states() -> void:
-	if is_on_floor() == true:
-		if velocity.x == 0:
-			set_state(PlayerState.IDLE)
-		else:
-			set_state(PlayerState.RUN)
-	else:
-		if velocity.y > 0:
-			set_state(PlayerState.FALL)
-		else:
-			set_state(PlayerState.JUMP)
+    if is_on_floor() == true:
+        if velocity.x == 0:
+            set_state(PlayerState.IDLE)
+        else:
+            set_state(PlayerState.RUN)
+    else:
+        if velocity.y > 0:
+            set_state(PlayerState.FALL)
+        else:
+            set_state(PlayerState.JUMP)
 
 func set_state(new_state: PlayerState) -> void:
-	if new_state == _state:
-		return
-		
-	_state = new_state
-	
-	match _state:
-		PlayerState.IDLE:
-			animated_sprite_2d.animation = "idle"
-		PlayerState.RUN:
-			animated_sprite_2d.animation = "run"
-		PlayerState.JUMP:
-			animated_sprite_2d.animation = "jump"
-		PlayerState.FALL:
-			animated_sprite_2d.animation = "fall"
+    if new_state == _state:
+        return
+        
+    _state = new_state
+    
+    match _state:
+        PlayerState.IDLE:
+            animated_sprite_2d.animation = "idle"
+        PlayerState.RUN:
+            animated_sprite_2d.animation = "run"
+        PlayerState.JUMP:
+            animated_sprite_2d.animation = "jump"
+        PlayerState.FALL:
+            animated_sprite_2d.animation = "fall"
 
+func handle_movement_input_behaviour() -> void:
+    if not input_enabled:
+            return
+
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
+
+    var direction := Input.get_axis("left", "right")
+    if direction:
+        velocity.x = direction * SPEED
+    else:
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func hit_slow_down() -> void:
+    if not got_hit:
+        return
+
+    velocity.x = move_toward(velocity.x, 0, 20.0)
 
 func _on_phase_end_body_entered(_body: Node2D) -> void:
-	get_tree().call_deferred("change_scene_to_file", phase_path)
+    get_tree().call_deferred("change_scene_to_file", phase_path)
 
 
-func _on_repel_spell_body_entered(_body: Node2D) -> void:
-	velocity = Vector2(-5000, -5)
+func _on_repel_spell_body_entered(body: Node2D) -> void:
+    if body.is_in_group("player"):
+        input_enabled = false
+        got_hit = true
+        velocity = Vector2(-500, -50)
