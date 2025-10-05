407db0c1c8dde3adef81b447d1fd2106d85b05a3
Anderson Job Loeffler <aloeffler.j@gmail.com>
2025-10-05 16:59:52 -0300

    feat: finish level 4

---
+++ scenes/lady_speech.gd
@@ -0,0 +1,8 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    await get_tree().create_timer(2).timeout
+    text = ""  # Clear the text
+
---
+++ scenes/speech-lvl4.gd
@@ -0,0 +1,9 @@
+extends RichTextLabel
+
+# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    await get_tree().create_timer(2).timeout
+    text = "[b][font_size=9][color=#a3c1b7][wave amp=30.0 freq=3.0 connected=1]I want to have a bike[/wave][/color][/font_size][/b]"  # Clear the text
+    await get_tree().create_timer(2).timeout
+    text = ""  # Clear the text
