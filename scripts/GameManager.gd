5f41a4a409ea14c7ba1b9fe201e959150fa30fa9
Júlio Cezar Koronati <jckoronati@hotmail.com>
2025-10-05 09:33:33 -0300

    test(game-manager): remove dumps at budget handle methods

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -25,12 +25,10 @@ func end_game() -> void:
 func increase_budget(value: int) -> void:
 	self.player_budget += value
 	budget_updated.emit(self.player_budget)
-	print("Your budget is $", self.player_budget, " dollars more")
 
 
 func increase_power_up_jump_multiply(multiply_value: float = 0.25) -> void:
 	self.power_up_jump_multiply += multiply_value
-	print("Jump updated to: ", self.power_up_jump_multiply)
 
 
 func reset_player_dialog_text() -> void:
