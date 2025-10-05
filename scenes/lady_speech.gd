a4c8635baefb5df07edae1ae088348564e82d3dc
Anderson Job Loeffler <aloeffler.j@gmail.com>
2025-10-05 17:25:48 -0300

    feat: adds boss

--- scenes/lady_speech.gd
+++ scenes/lady_speech.gd
@@ -3,6 +3,6 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    await get_tree().create_timer(2).timeout
+    await get_tree().create_timer(4).timeout
     text = ""  # Clear the text
 
