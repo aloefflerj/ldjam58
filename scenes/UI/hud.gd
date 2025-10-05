9e5ebeaf4ffc4cc8a529a45d986ad9835359f283
Júlio Cezar Koronati <jckoronati@hotmail.com>
2025-10-05 15:15:23 -0300

    feat(end-game): change game over screen through a button at hud

--- scenes/UI/hud.gd
+++ scenes/UI/hud.gd
@@ -2,6 +2,9 @@ extends CanvasLayer
 
 
 func _ready() -> void:
+	# TODO make it show in a dynamic way
+	# $Retry.hide()
+	
 	GameManager.budget_updated.connect(on_budget_updated)
 	
 	on_budget_updated(GameManager.player_budget)
@@ -9,3 +12,7 @@ func _ready() -> void:
 
 func on_budget_updated(new_budget: int) -> void:
 	$BudgeLabel.text = "Budget: $" + str(new_budget)
+
+
+func _on_retry_pressed() -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/Game.tscn")
--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -6,7 +6,6 @@ signal budget_updated(new_budget)
 const initial_dialog_text_message: String = "ERROR!"
 
 var soundtrack_game_scene = preload("res://scenes/Itens/soundtrack.tscn")
-var game_over_scene = preload("res://scenes/game_over.tscn")
 var player_budget: int = 0
 var power_up_jump_multiply: float = 1.00
 var player_dialog_text: String = self.initial_dialog_text_message
@@ -17,8 +16,7 @@ func _ready() -> void:
 
 
 func end_game() -> void:
-	var game_over_instance = game_over_scene.instantiate()
-	get_tree().root.add_child(game_over_instance)
+	pass
 
 
 func increase_budget(value: int) -> void:
